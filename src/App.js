81458446f6d06b04bfb893b4e808c971eca21f54
João Victor Medeiros <jvmedeiros03@gmail.com>
2026-01-20 19:56:27 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import HomePage from './pages/HomePage/HomePage';
-import ProductListingPage from './pages/ProductListingPage/ProductListingPage';
-import CategoryPage from './pages/CategoryPage/CategoryPage';
-import MyOrdersPage from './pages/MyOrdersPage/MyOrdersPage';
 import './App.css';
 
 function App() {
@@ -13,9 +10,6 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/produtos" element={<ProductListingPage />} />
-          <Route path="/categorias" element={<CategoryPage />} />
-          <Route path="/meus-pedidos" element={<MyOrdersPage />} />
         </Routes>
       </Layout>
     </Router>
--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './logo.css';
 
 const Logo = () => {
   return (
     <div className="logo">
-      <h2>Digital Store</h2>
+    
     </div>
   );
 };
